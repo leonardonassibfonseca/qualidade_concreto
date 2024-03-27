049dbd4def9f9743c87372333d067362aca46cca
LEONARDO NASSIB FONSECA <141882326+leonardonassibfonseca@users.noreply.github.com>
2024-03-27 18:33:19 -0300

    Update organizador.py

--- organizador.py
+++ organizador.py
@@ -15,14 +15,14 @@ class Projeto:
         self.water_mms = pickle.load(open(self.home_path + 'transformacoes_water_mms.pkl', 'rb'))
 
     def transformacao_dos_dados(self, df5):
-        df5['age'] = self.transformacoes_age_mms.transform(df5[['age']].values)
-        df5['cement'] = self.transformacoes_cement_mms.transform(df5[['cement']].values)
-        df5['coarseaggregate'] = self.transformacoes_coarseaggregate_mms.transform(df5[['coarseaggregate']].values)
-        df5['fineaggregate'] = self.transformacoes_fineaggregate_mms.transform(df5[['fineaggregate']].values)
-        df5['flyash'] = self.transformacoes_flyash_mms.transform(df5[['flyash']].values)
-        df5['slag'] = self.transformacoes_slag_mms.transform(df5[['slag']].values)
-        df5['superplasticizer'] = self.transformacoes_superplasticizer_mms.transform(df5[['superplasticizer']].values)
-        df5['water'] = self.transformacoes_water_mms.transform(df5[['water']].values)
+        df5['age'] = self.age_mms.transform(df5[['age']].values)
+        df5['cement'] = self.cement_mms.transform(df5[['cement']].values)
+        df5['coarseaggregate'] = self.coarseaggregate_mms.transform(df5[['coarseaggregate']].values)
+        df5['fineaggregate'] = self.fineaggregate_mms.transform(df5[['fineaggregate']].values)
+        df5['flyash'] = self.flyash_mms.transform(df5[['flyash']].values)
+        df5['slag'] = self.slag_mms.transform(df5[['slag']].values)
+        df5['superplasticizer'] = self.superplasticizer_mms.transform(df5[['superplasticizer']].values)
+        df5['water'] = self.water_mms.transform(df5[['water']].values)
 
     def get_prediction(self, modelo, dados_original, dados_teste):
         pred = modelo.predict(dados_teste)
