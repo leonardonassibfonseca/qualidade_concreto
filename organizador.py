12f80a34afdb7832103534dc9a520d54e8b1433b
LEONARDO NASSIB FONSECA <141882326+leonardonassibfonseca@users.noreply.github.com>
2024-03-27 14:57:16 -0300

    Update organizador.py

--- organizador.py
+++ organizador.py
@@ -1,30 +1,28 @@
 import pickle
 import pandas                                  as pd
 import numpy                                   as np
-from google.cloud          import storage
-from sklearn.preprocessing import MinMaxScaler
 
 class Projeto:
     def __init__(self):
         self.home_path = '/transformacoes/'
-        self.age_mms = pickle.load(open(self.home_path + 'transformacoes/age_mms.pkl', 'rb'))
-        self.cement_mms = pickle.load(open(self.home_path + 'armazenamento-qualidade-concreto/transformacoes/cement_mms.pkl', 'rb'))
-        self.coarseaggregate_mms = pickle.load(open(self.home_path + 'armazenamento-qualidade-concreto/transformacoes/coarseaggregate_mms.pkl', 'rb'))
-        self.fineaggregate_mms = pickle.load(open(self.home_path + 'armazenamento-qualidade-concreto/transformacoes/fineaggregate_mms.pkl', 'rb'))
-        self.flyash_mms = pickle.load(open(self.home_path + 'armazenamento-qualidade-concreto/transformacoes/flyash_mms.pkl', 'rb'))
-        self.slag_mms = pickle.load(open(self.home_path + 'armazenamento-qualidade-concreto/transformacoes/slag_mms.pkl', 'rb'))
-        self.superplasticizer_mms = pickle.load(open(self.home_path + 'armazenamento-qualidade-concreto/transformacoes/superplasticizer_mms.pkl', 'rb'))
-        self.water_mms = pickle.load(open(self.home_path + 'armazenamento-qualidade-concreto/transformacoes/water_mms.pkl', 'rb'))
+        self.age_mms = pickle.load(open(self.home_path + 'transformacoes/transformacoes_age_mms.pkl', 'rb'))
+        self.cement_mms = pickle.load(open(self.home_path + 'transformacoes/transformacoes_cement_mms.pkl', 'rb'))
+        self.coarseaggregate_mms = pickle.load(open(self.home_path + 'transformacoes/transformacoes_coarseaggregate_mms.pkl', 'rb'))
+        self.fineaggregate_mms = pickle.load(open(self.home_path + 'transformacoes/transformacoes_fineaggregate_mms.pkl', 'rb'))
+        self.flyash_mms = pickle.load(open(self.home_path + 'transformacoes/transformacoes_flyash_mms.pkl', 'rb'))
+        self.slag_mms = pickle.load(open(self.home_path + 'transformacoes/transformacoes_slag_mms.pkl', 'rb'))
+        self.superplasticizer_mms = pickle.load(open(self.home_path + 'transformacoes/transformacoes_superplasticizer_mms.pkl', 'rb'))
+        self.water_mms = pickle.load(open(self.home_path + 'transformacoes/transformacoes_water_mms.pkl', 'rb'))
 
     def transformacao_dos_dados(self, df5):
-        df5['age'] = self.age_mms.transform(df5[['age']].values)
-        df5['cement'] = self.cement_mms.transform(df5[['cement']].values)
-        df5['coarseaggregate'] = self.coarseaggregate_mms.transform(df5[['coarseaggregate']].values)
-        df5['fineaggregate'] = self.fineaggregate_mms.transform(df5[['fineaggregate']].values)
-        df5['flyash'] = self.flyash_mms.transform(df5[['flyash']].values)
-        df5['slag'] = self.slag_mms.transform(df5[['slag']].values)
-        df5['superplasticizer'] = self.superplasticizer_mms.transform(df5[['superplasticizer']].values)
-        df5['water'] = self.water_mms.transform(df5[['water']].values)
+        df5['age'] = self.transformacoes_age_mms.transform(df5[['age']].values)
+        df5['cement'] = self.transformacoes_cement_mms.transform(df5[['cement']].values)
+        df5['coarseaggregate'] = self.transformacoes_coarseaggregate_mms.transform(df5[['coarseaggregate']].values)
+        df5['fineaggregate'] = self.transformacoes_fineaggregate_mms.transform(df5[['fineaggregate']].values)
+        df5['flyash'] = self.transformacoes_flyash_mms.transform(df5[['flyash']].values)
+        df5['slag'] = self.transformacoes_slag_mms.transform(df5[['slag']].values)
+        df5['superplasticizer'] = self.transformacoes_superplasticizer_mms.transform(df5[['superplasticizer']].values)
+        df5['water'] = self.transformacoes_water_mms.transform(df5[['water']].values)
 
     def get_prediction(self, modelo, dados_original, dados_teste):
         pred = modelo.predict(dados_teste)
