4f70900b5c828b9476a6e74a6e0ba702cbe0a74f
LEONARDO NASSIB FONSECA <141882326+leonardonassibfonseca@users.noreply.github.com>
2024-04-08 16:27:06 -0300

    Add files via upload

---
+++ api_handler.py
@@ -0,0 +1,47 @@
+import pickle
+import pandas as pd
+from flask import Flask, request, Response
+
+#Diretório onde está salvo a API handler.py
+#Nome do arquivo.py
+#Nome da classe que foi criada dentro do arquivo.py
+
+#from Diretório.arquivo        import classe
+from script import Projeto
+
+# Carregando o modelo treinado usando pickle
+modelo = pickle.load(open('/home/leonardo/projetos_/qualidade_concreto/modelo/modelo_treinado.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    teste_json = request.get_json() #Recebe um arquivo JSON a partir da request
+
+    if teste_json:  #Se o teste_json for diferente de vazio, ou seja, se foi carregado algum dado
+        #Verifica se o arquivo passado é um tipo de dicionário e se sim, foi enviado um arquivo com somente uma linha
+        if isinstance(teste_json, dict):
+            #Cria um dataframe e para isso é necessário indicar no Pandas qual é o nº da linha inicial, nesta caso, 0
+            dados_originais = pd.DataFrame(teste_json, index = [0])
+        else:
+            #Se não for é um dicionário, foi enviado um arquivo com mais de uma linha
+            dados_originais = pd.DataFrame(teste_json, columns = teste_json[0].keys())
+
+        #Instanciando a classe do projeto
+        pipeline = Projeto()
+
+        #Transformação dos dados
+        df5 = pipeline.transformacao_dos_dados(dados_originais)
+        
+        #predição
+        df_resposta = pipeline.get_prediction(modelo, dados_originais, df5)
+        
+        return df_resposta
+    else:
+        return Response('{}', status = 200, mimetype = 'application/json')
+
+if __name__ == '__main__':
+    #Dizer para endpoint rodar no localhost (rodando na máquina)
+    #port = os.environ.get('PORT', 5000)
+    #app.run('0.0.0.0', port = port)
+    app.run('0.0.0.0', debug = True)
---
+++ script.py
@@ -0,0 +1,34 @@
+import pickle
+import pandas                                  as pd
+import numpy                                   as np
+
+class Projeto:
+    def __init__(self):
+        self.home_path = '/home/leonardo/projetos_/qualidade_concreto/transformacoes/'
+        self.age_mms = pickle.load(open(self.home_path + 'age_mms.pkl', 'rb'))
+        self.cement_mms = pickle.load(open(self.home_path + 'cement_mms.pkl', 'rb'))
+        self.coarseaggregate_mms = pickle.load(open(self.home_path + 'coarseaggregate_mms.pkl', 'rb'))
+        self.fineaggregate_mms = pickle.load(open(self.home_path + 'fineaggregate_mms.pkl', 'rb'))
+        self.flyash_mms = pickle.load(open(self.home_path + 'flyash_mms.pkl', 'rb'))
+        self.slag_mms = pickle.load(open(self.home_path + 'slag_mms.pkl', 'rb'))
+        self.superplasticizer_mms = pickle.load(open(self.home_path + 'superplasticizer_mms.pkl', 'rb'))
+        self.water_mms = pickle.load(open(self.home_path + 'water_mms.pkl', 'rb'))
+
+    def transformacao_dos_dados(self, df5):
+        df5['age'] = self.age_mms.transform(df5[['age']].values)
+        df5['cement'] = self.cement_mms.transform(df5[['cement']].values)
+        df5['coarseaggregate'] = self.coarseaggregate_mms.transform(df5[['coarseaggregate']].values)
+        df5['fineaggregate'] = self.fineaggregate_mms.transform(df5[['fineaggregate']].values)
+        df5['flyash'] = self.flyash_mms.transform(df5[['flyash']].values)
+        df5['slag'] = self.slag_mms.transform(df5[['slag']].values)
+        df5['superplasticizer'] = self.superplasticizer_mms.transform(df5[['superplasticizer']].values)
+        df5['water'] = self.water_mms.transform(df5[['water']].values)
+
+        #Colunas selecionadas a partir a importância das variáveis
+        colunas_selecionadas = ['cement', 'age']
+        return df5[colunas_selecionadas]
+
+    def get_prediction(self, modelo, dados_original, dados_teste):
+        pred = modelo.predict(dados_teste)
+        dados_original['Score'] = pred
+        return dados_original.to_json(orient = 'records', date_format = 'iso')
